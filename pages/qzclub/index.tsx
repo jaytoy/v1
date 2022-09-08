70dd24dd15446e2fc9f9179ed0627d2b0a4c5dba
Jay Toy <jay_toyjianying@hotmail.com>
2022-09-08 17:33:57 +0200

    add new pages

---
+++ pages/qzclub/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Footer from '../../components/footer'
+import Header from '../../components/header'
+
+function Index() {
+  return (
+    <div className="bg-white">
+      <div className="relative overflow-hidden">
+        <Header />
+
+        <main>
+          
+        </main>
+
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Index
